d3a8479b8427baf7409adb8a27aa818e381f444e
roman gasparini <roman.gasparini@gmail.com>
2026-09-13 01:36:11 -0300

    SC-003: create delete dialog

--- superheroes-challenge/src/app/features/superheros/pages/superheros-page/superheros-page.component.ts
+++ superheroes-challenge/src/app/features/superheros/pages/superheros-page/superheros-page.component.ts
@@ -1,5 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { SuperherosListComponent } from '../../superheros-list/superheros-list.component';
+import { superheroStore } from '../../store/superhero.store';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../../../shared/dialog/dialog.component';
 
 @Component({
   selector: 'app-superheros-page',
@@ -8,4 +16,28 @@ import { SuperherosListComponent } from '../../superheros-list/superheros-list.c
   styleUrl: './superheros-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SuperherosPageComponent {}
+export class SuperherosPageComponent implements OnInit {
+  public readonly superheroStore = inject(superheroStore);
+  private readonly dialog = inject(MatDialog);
+
+  ngOnInit(): void {
+    this.superheroStore.loadSuperheros();
+  }
+
+  openDeleteDialog(id: string) {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '350px',
+      data: {
+        title: 'Eliminar superheroe',
+        confirmText: 'Eliminar',
+        cancelText: 'Aceptar',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.superheroStore.deleteSuperhero(id);
+      }
+    });
+  }
+}
